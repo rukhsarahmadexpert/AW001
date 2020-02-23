eb15e01d3d9b35174b91401f11b77d8079a7ed8f
Rizwan Afridi <prince.alyan99@gmail.com>
2020-02-23 10:02:24 +0400

    Rizwan changes

--- IT.Core/ViewModels/FuelPricesViewModel.cs
+++ IT.Core/ViewModels/FuelPricesViewModel.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace IT.Core.ViewModels
 {
-  public class FuelPricesViewModel
-    {
-        public int Id { get; set; }
-        public decimal Price  { get; set; }
-        public int Unit { get; set; }
-        public string UnitName { get; set; }
-        public string Month  { get; set; }
-        public int CreatedBy  { get; set; }
-        public string CreatedDate  { get; set; }
-        public int UpdatedBy  { get; set; }
-        public string UserName  { get; set; }
-        public bool IsActive  { get; set; }
-    }
+      public class FuelPricesViewModel
+      {
+            public int Id { get; set; }
+            public decimal Price  { get; set; }
+            public int Unit { get; set; }
+            public string UnitName { get; set; }
+            public string Month  { get; set; }
+            public int CreatedBy  { get; set; }
+            public string CreatedDate  { get; set; }
+            public int UpdatedBy  { get; set; }
+            public string UserName  { get; set; }
+            public bool IsActive  { get; set; }
+      }
 }
