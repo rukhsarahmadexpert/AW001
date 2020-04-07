5aaa4ac948ecdbddc09c1e374e6daf2dd0989c97
Rukhsar Ahmad <itmolen1@gmail.com>
2020-04-07 15:58:08 +0400

    rukhsar changes

--- IT.Web/Controllers/UserController.cs
+++ IT.Web/Controllers/UserController.cs
@@ -11,7 +11,7 @@ using System.Web.Script.Serialization;
 
 namespace IT.Web_New.Controllers
 {
-    [ExceptionLog]   
+    [ExceptionLog]
     public class UserController : Controller
     {
         WebServices webServices = new WebServices();
@@ -51,7 +51,7 @@ namespace IT.Web_New.Controllers
             {
                 throw ex;
             }
-         
+
         }
 
         [HttpGet]
@@ -81,55 +81,55 @@ namespace IT.Web_New.Controllers
         }
 
         [HttpPost]
-        public ActionResult UserInformationUpdate(UserViewModel userViewModel,  HttpPostedFileBase ImageUrl1)
+        public ActionResult UserInformationUpdate(UserViewModel userViewModel, HttpPostedFileBase ImageUrl1)
         {
             try
             {
-               
-                    //if (Request.Files.Count > 0 && LogoUrl != null)
-                    //{
-                    var file = ImageUrl1;
 
-                    using (HttpClient client = new HttpClient())
+                //if (Request.Files.Count > 0 && LogoUrl != null)
+                //{
+                var file = ImageUrl1;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    using (var content = new MultipartFormDataContent())
                     {
-                        using (var content = new MultipartFormDataContent())
+                        if (ImageUrl1 != null)
                         {
-                            if (ImageUrl1 != null)
-                            {
-                                byte[] fileBytes = new byte[file.InputStream.Length + 1];
-                                file.InputStream.Read(fileBytes, 0, fileBytes.Length);
-                                var fileContent = new ByteArrayContent(fileBytes);
-                                fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("ImageUrl") { FileName = file.FileName };
-                                content.Add(fileContent);
-                            }
-                            else
+                            byte[] fileBytes = new byte[file.InputStream.Length + 1];
+                            file.InputStream.Read(fileBytes, 0, fileBytes.Length);
+                            var fileContent = new ByteArrayContent(fileBytes);
+                            fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("ImageUrl") { FileName = file.FileName };
+                            content.Add(fileContent);
+                        }
+                        else
                         {
                             content.Add(new StringContent(userViewModel.ImageUrl ?? ""), "ImageUrl");
                         }
-                            content.Add(new StringContent("ClientDocs"), "ClientDocs");
-                            content.Add(new StringContent(userViewModel.UserId.ToString()), "UserId");
-                            content.Add(new StringContent(userViewModel.FullName ?? ""), "FullName");
-                            content.Add(new StringContent(userViewModel.UserName ?? ""), "UserName");
-                            content.Add(new StringContent(userViewModel.Gender ?? ""), "Gender");
-                            content.Add(new StringContent(userViewModel.DOB.ToString() ?? System.DateTime.Now.ToString()), "DOB");
+                        content.Add(new StringContent("ClientDocs"), "ClientDocs");
+                        content.Add(new StringContent(userViewModel.UserId.ToString()), "UserId");
+                        content.Add(new StringContent(userViewModel.FullName ?? ""), "FullName");
+                        content.Add(new StringContent(userViewModel.UserName ?? ""), "UserName");
+                        content.Add(new StringContent(userViewModel.Gender ?? ""), "Gender");
+                        content.Add(new StringContent(userViewModel.DOB.ToString() ?? System.DateTime.Now.ToString()), "DOB");
 
 
-                            
-                            var result = webServices.PostMultiPart(content, "User/UserInformationUpdate", true);
-                            if (result.StatusCode == System.Net.HttpStatusCode.Accepted)
-                            {
+
+                        var result = webServices.PostMultiPart(content, "User/UserInformationUpdate", true);
+                        if (result.StatusCode == System.Net.HttpStatusCode.Accepted)
+                        {
 
                             userViewModel = (new JavaScriptSerializer().Deserialize<UserViewModel>(result.Data.ToString()));
 
                             //return RedirectToAction("/");
                             return RedirectToAction(nameof(UserInformation));
                         }
-                            else
-                            {
-                                ViewBag.Message = "Failed";
-                            }
+                        else
+                        {
+                            ViewBag.Message = "Failed";
                         }
                     }
+                }
 
                 return RedirectToAction(nameof(UserInformation));
             }
@@ -149,12 +149,12 @@ namespace IT.Web_New.Controllers
             {
                 // DeviceId = System.Net.Dns.GetHostName().ToString()
                 //  DeviceId = System.Environment.GetEnvironmentVariable("COMPUTERNAME").ToString()
-               // DeviceId = System.Web.HttpContext.Current.Server.MachineName;
+                // DeviceId = System.Web.HttpContext.Current.Server.MachineName;
                 //DeviceId = Request.UserHostAddress
                 DeviceId = Request.Browser.Id
             };
-            var result = webServices.Post(loginViewModel, "User/LogOut", false);           
-            return Redirect(nameof(Login));           
+            var result = webServices.Post(loginViewModel, "User/LogOut", false);
+            return Redirect(nameof(Login));
         }
 
         [HttpGet]
@@ -172,6 +172,11 @@ namespace IT.Web_New.Controllers
         [HttpGet]
         public ActionResult Login()
         {
+            if (TempData["SuccessPWD"] != null)
+            {
+                ViewBag.success = TempData["SuccessPWD"];
+            }
+
             return View(new LoginViewModel());
         }
 
@@ -305,7 +310,7 @@ namespace IT.Web_New.Controllers
 
                 return View(changePasswordViewModel);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw ex;
             }
@@ -336,12 +341,12 @@ namespace IT.Web_New.Controllers
                         var Result = (new JavaScriptSerializer()).Deserialize<string>(result.Data.ToString());
                         return RedirectToAction(nameof(UserInformation));
                     }
-                    else if(result.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    else if (result.StatusCode == System.Net.HttpStatusCode.NotFound)
                     {
                         ModelState.AddModelError("Error", "Email not found");
                         return View(changePasswordViewModel);
                     }
-                    
+
                     return View(changePasswordViewModel);
                 }
             }
@@ -364,5 +369,160 @@ namespace IT.Web_New.Controllers
                 throw ex;
             }
         }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(LoginViewModel loginViewModel)
+        {
+            try
+            {
+                ModelState.Remove("Password");
+                if (!ModelState.IsValid)
+                {
+                    return View(loginViewModel);
+                }
+                else
+                {
+                    var result = webServices.Post(loginViewModel, "User/ForgotPassword", false);
+
+                    if (result.StatusCode == System.Net.HttpStatusCode.Accepted)
+                    {
+                        var data = (new JavaScriptSerializer().Deserialize<SearchModelForRecover>(result.Data.ToString()));
+
+                        loginViewModel.Device = data.searchkey;
+
+                        Session["loginViewModel"] = loginViewModel;
+
+                        TempData["Success"] = "we have send password reset code to your email, please check inbox or spam";
+
+                        return View("PasswordRestCode", new SearchViewModel());
+                    }
+
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult PasswordRestCode()
+        {
+            return View(new SearchViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult PasswordRestCode(SearchViewModel searchViewModel)
+        {
+            try
+            {
+                if (Session["loginViewModel"] != null)
+                {
+                    if (searchViewModel.SearchKey2 != null || searchViewModel.SearchKey2 != "")
+                    {
+                        searchViewModel.SearchKey2 = searchViewModel.SearchKey2.Replace(" ", "");
+                    }
+
+                    var loginViewModel = new LoginViewModel();
+                    loginViewModel = Session["loginViewModel"] as LoginViewModel;
+
+                    if (loginViewModel.Device == searchViewModel.SearchKey2)
+                    {
+                        UserViewModel userViewModel = new UserViewModel();
+                        userViewModel.UserName = loginViewModel.UserName;
+                       return RedirectToAction(nameof(UpdateForGotPassword));                       
+                    }
+                    else
+                    {
+                        TempData["Success"] = "Incorrect Code, Please enter the accurate code";
+                        return View(searchViewModel);
+                    }
+                }
+                else
+                {
+                    TempData["Success"] = "Code has been expired";
+                    return View(searchViewModel);
+                }                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }           
+        }
+
+        [HttpGet]
+        public ActionResult UpdateForGotPassword()
+        {
+            try
+            {
+                var loginViewModel = new LoginViewModel();
+                loginViewModel = Session["loginViewModel"] as LoginViewModel;
+
+                UserViewModel userViewModel = new UserViewModel();
+                userViewModel.UserName = loginViewModel.UserName;
+                
+                return View(userViewModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult UpdateForGotPassword(UserViewModel userViewModel)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                {
+                    ViewBag.success = "Password and Confirm Password not matched";
+                    return View(userViewModel);
+                }
+                else
+                {
+                    ChangePasswordViewModel changePasswordViewModel = new ChangePasswordViewModel();
+                    changePasswordViewModel.Email = userViewModel.UserName;
+                    changePasswordViewModel.NewPassword = userViewModel.Password;
+
+                    var Result = webServices.Post(changePasswordViewModel, "User/UpdatePassword");
+                    if(Result.StatusCode == System.Net.HttpStatusCode.Accepted)
+                    {
+                        if(Result.Data == "\"Password chnage successfully\"")
+                        {
+                            TempData["SuccessPWD"] = "Password change successfully";
+                            return RedirectToAction(nameof(Login));
+                        }
+                    }
+                }
+
+                ViewBag.success = "Filed to update password, Please try again later";
+                return View(userViewModel);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+
+    public class SearchModelForRecover
+    {       
+        public int Id { get; set; }
+        public string searchkey { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public int CompanyId { get; set; }
+        public bool Status { get; set; }
+        public string Flage { get; set; }
+        public string DeviceTiken { get; set; }
+        public string DeviceId { get; set; }
+        public string CompanyName { get; set; }
+        public string NotificationCode { get; set; }
+        public int Quantity { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    
     }
 }
