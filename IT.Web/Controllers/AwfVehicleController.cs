c2f67d5946c0e222eb4d15aa45c3aa6657a38cb7
Rukhsar Ahmad <itmolen1@gmail.com>
2020-04-14 12:32:31 +0400

    Rukhsar changes

--- IT.Web/Controllers/AwfVehicleController.cs
+++ IT.Web/Controllers/AwfVehicleController.cs
@@ -32,7 +32,7 @@ namespace IT.Web_New.Controllers
                 CompanyId = Convert.ToInt32(Session["CompanyId"]);
 
                 PagingParameterModel pagingParameterModel = new PagingParameterModel
-                { 
+                {
                     pageNumber = 1,
                     _pageSize = 1,
                     CompanyId = CompanyId,
@@ -74,7 +74,7 @@ namespace IT.Web_New.Controllers
         }
 
         [HttpPost]
-        public ActionResult ChangeStatus(VehicleViewModel  vehicleViewModel)
+        public ActionResult ChangeStatus(VehicleViewModel vehicleViewModel)
         {
             try
             {
@@ -118,38 +118,39 @@ namespace IT.Web_New.Controllers
                 }
                 else
                 {
-                    if (Request.Files.Count > 0)
+                    using (HttpClient client = new HttpClient())
                     {
-                        HttpPostedFileBase[] httpPostedFileBase = new HttpPostedFileBase[4];
-                        if (vehicleViewModel.MulkiaFront1File != null)
+                        using (var content = new MultipartFormDataContent())
                         {
-                            httpPostedFileBase[0] = vehicleViewModel.MulkiaFront1File;
-                        }
-                        if (vehicleViewModel.MulkiaBack1File != null)
-                        {
-                            httpPostedFileBase[1] = vehicleViewModel.MulkiaBack1File;
-                        }
-                        if (vehicleViewModel.MulkiaFront2File != null)
-                        {
-                            httpPostedFileBase[2] = vehicleViewModel.MulkiaFront2File;
-                        }
-                        if (vehicleViewModel.MulkiaBack2File != null)
-                        {
-                            httpPostedFileBase[3] = vehicleViewModel.MulkiaBack2File;
-                        }
-
-                        var file = vehicleViewModel.MulkiaFront1File;
-
-                        using (HttpClient client = new HttpClient())
-                        {
-                            using (var content = new MultipartFormDataContent())
+                            if (Request.Files.Count > 0)
                             {
+                                HttpPostedFileBase[] httpPostedFileBase = new HttpPostedFileBase[4];
+                                if (vehicleViewModel.MulkiaFront1File != null)
+                                {
+                                    httpPostedFileBase[0] = vehicleViewModel.MulkiaFront1File;
+                                }
+                                if (vehicleViewModel.MulkiaBack1File != null)
+                                {
+                                    httpPostedFileBase[1] = vehicleViewModel.MulkiaBack1File;
+                                }
+                                if (vehicleViewModel.MulkiaFront2File != null)
+                                {
+                                    httpPostedFileBase[2] = vehicleViewModel.MulkiaFront2File;
+                                }
+                                if (vehicleViewModel.MulkiaBack2File != null)
+                                {
+                                    httpPostedFileBase[3] = vehicleViewModel.MulkiaBack2File;
+                                }
+
+
                                 if (httpPostedFileBase.ToList().Count > 0)
                                 {
                                     for (int i = 0; i < 4; i++)
                                     {
                                         if (httpPostedFileBase[i] != null)
                                         {
+                                            var file = vehicleViewModel.MulkiaFront1File;
+
                                             file = httpPostedFileBase[i];
 
                                             byte[] fileBytes = new byte[file.InputStream.Length + 1];
@@ -176,34 +177,59 @@ namespace IT.Web_New.Controllers
                                         }
                                     }
                                 }
+                            }
 
-                                string UserId = Session["UserId"].ToString();
-                                content.Add(new StringContent(UserId), "CreatedBy");
-                                CompanyId = Convert.ToInt32(Session["CompanyId"]);
-                                content.Add(new StringContent(CompanyId.ToString()), "CompanyId");
-                                content.Add(new StringContent("ClientDocs"), "ClientDocs");
-                                content.Add(new StringContent(vehicleViewModel.VehicleType.ToString()), "VehicleType");
-                                content.Add(new StringContent(vehicleViewModel.TraficPlateNumber ?? ""), "TraficPlateNumber");
-                                content.Add(new StringContent(vehicleViewModel.TCNumber ?? ""), "TCNumber");
-                                content.Add(new StringContent(vehicleViewModel.Model ?? ""), "Model");
-                                content.Add(new StringContent(vehicleViewModel.Brand ?? ""), "Brand");
-                                content.Add(new StringContent(vehicleViewModel.Color ?? ""), "Color");
-                                content.Add(new StringContent(vehicleViewModel.MulkiaExpiry ?? ""), "MulkiaExpiry");
-                                content.Add(new StringContent(vehicleViewModel.InsuranceExpiry ?? ""), "InsuranceExpiry");
-                                content.Add(new StringContent(vehicleViewModel.RegisteredRegion ?? ""), "RegisteredRegion");
-                                content.Add(new StringContent(vehicleViewModel.Comments ?? ""), "Comments");
+                            string UserId = Session["UserId"].ToString();
+                            content.Add(new StringContent(UserId ?? "1"), "CreatedBy");
+                            CompanyId = Convert.ToInt32(Session["CompanyId"]);
+                            content.Add(new StringContent(CompanyId.ToString()), "CompanyId");
+                            content.Add(new StringContent("ClientDocs"), "ClientDocs");
+                            content.Add(new StringContent(vehicleViewModel.VehicleType.ToString()), "VehicleType");
+                            content.Add(new StringContent(vehicleViewModel.TraficPlateNumber ?? ""), "TraficPlateNumber");
+                            content.Add(new StringContent(vehicleViewModel.TCNumber ?? ""), "TCNumber");
+                            content.Add(new StringContent(vehicleViewModel.Model ?? ""), "Model");
+                            content.Add(new StringContent(vehicleViewModel.Brand ?? ""), "Brand");
+                            content.Add(new StringContent(vehicleViewModel.Color ?? ""), "Color");
+                            content.Add(new StringContent(vehicleViewModel.MulkiaExpiry ?? ""), "MulkiaExpiry");
+                            content.Add(new StringContent(vehicleViewModel.InsuranceExpiry ?? ""), "InsuranceExpiry");
+                            content.Add(new StringContent(vehicleViewModel.RegisteredRegion ?? ""), "RegisteredRegion");
+                            content.Add(new StringContent(vehicleViewModel.Comments ?? ""), "Comments");
 
-                                var result = webServices.PostMultiPart(content, "AWFVehicle/Add", true);
-                                if (result.StatusCode == System.Net.HttpStatusCode.Accepted)
+                            var result = webServices.PostMultiPart(content, "AWFVehicle/Add", true);
+                            if (result.StatusCode == System.Net.HttpStatusCode.Accepted)
+                            {
+                                if (result.Data != "TraficPlateNumber Already Availible")
                                 {
-                                    return Redirect(nameof(Index));
+                                    VehicleTypeController vehicleTypeController = new VehicleTypeController();
+                                    ViewBag.VehicleTypes = vehicleTypeController.VehicleTypes();
+
+                                    ModelState.AddModelError("TraficPlateNumber", "TraficPlateNumber Already Availible, Choose another");
+
+                                    return View(vehicleViewModel);
                                 }
+                                else
+                                {
+                                    return RedirectToAction(nameof(Index));
+                                }
+
+                            }
+                            else if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                            {
+                                VehicleTypeController vehicleTypeController = new VehicleTypeController();
+                                ViewBag.VehicleTypes = vehicleTypeController.VehicleTypes();
+
+                                ModelState.AddModelError("TraficPlateNumber", "TraficPlateNumber Already Availible, Choose another");
+
+                                return View(vehicleViewModel);
+                            }
+                            else
+                            {
+                                return RedirectToAction(nameof(Index));
                             }
                         }
                     }
-                    return Redirect(nameof(Index));
-                    //return RedirectToAction(nameof(Details), new { vehicleViewModel.Id });
                 }
+
             }
             catch (Exception ex)
             {
@@ -370,7 +396,7 @@ namespace IT.Web_New.Controllers
                 throw ex;
             }
         }
-        
+
         [NonAction]
         public List<VehicleViewModel> AdminVehicles()
         {
@@ -405,44 +431,44 @@ namespace IT.Web_New.Controllers
         {
             try
             {
-                if (Request.Files.Count > 0)
+                using (var content = new MultipartFormDataContent())
                 {
-                    var file = FileUrl;
-
                     using (HttpClient client = new HttpClient())
                     {
-                        using (var content = new MultipartFormDataContent())
+                        if (Request.Files.Count > 0)
                         {
+                            var file = FileUrl;
+
+
                             byte[] fileBytes = new byte[file.InputStream.Length + 1];
                             file.InputStream.Read(fileBytes, 0, fileBytes.Length);
                             var fileContent = new ByteArrayContent(fileBytes);
                             fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("FileUrl") { FileName = file.FileName };
                             content.Add(fileContent);
+
                             content.Add(new StringContent("ClientDocs"), "ClientDocs");
-                            string UserId = Session["UserId"].ToString();
-                            content.Add(new StringContent(UserId), "CreatedBy");
-                            content.Add(new StringContent(uploadDocumentsViewModel.VehicleId.ToString()), "VehicleId");
-                            content.Add(new StringContent(uploadDocumentsViewModel.FilesName ?? ""), "FilesName");
-                            var result = webServices.PostMultiPart(content, "UploadDocuments/UploadDocumentsAdd", true);
-                            if (result.StatusCode == System.Net.HttpStatusCode.Accepted)
-                            {
-                                return Redirect(nameof(Index));
-                            }
-                            else
-                            {
-                                return Redirect(nameof(Index));
-                            }
+                        }
+                        string UserId = Session["UserId"].ToString();
+                        content.Add(new StringContent(UserId), "CreatedBy");
+                        content.Add(new StringContent(uploadDocumentsViewModel.VehicleId.ToString()), "VehicleId");
+                        content.Add(new StringContent(uploadDocumentsViewModel.FilesName ?? ""), "FilesName");
+                        var result = webServices.PostMultiPart(content, "UploadDocuments/UploadDocumentsAdd", true);
+                        if (result.StatusCode == System.Net.HttpStatusCode.Accepted)
+                        {
+                            return Redirect(nameof(Index));
+                        }
+                        else
+                        {
+                            return Redirect(nameof(Index));
                         }
                     }
+
                 }
-                return View();
             }
             catch (Exception ex)
             {
                 throw ex;
             }
         }
-
-
     }
 }
