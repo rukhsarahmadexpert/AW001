5157b450e98085cd9f812a4704f6cc57cd300cea
Rizwan Afridi <prince.alyan99@gmail.com>
2020-04-20 12:58:00 +0400

    Rizwan chnages

--- IT.Web/Controllers/UserController.cs
+++ IT.Web/Controllers/UserController.cs
@@ -384,6 +384,7 @@ namespace IT.Web_New.Controllers
                 }
 
                 ViewBag.userViewModel = userViewModel;
+                changePasswordViewModel.Email = userViewModel.UserName;
 
                 return View(changePasswordViewModel);
             }
